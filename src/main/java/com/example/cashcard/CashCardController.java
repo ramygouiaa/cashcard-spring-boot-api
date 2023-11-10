a320c6da82ca1e2a03fce58f8950ae7f213c117d
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-11-10 20:03:36 +0100

    refactor CashCardController to get data from database H2

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -6,16 +6,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
          /* Here would be the code to retrieve the CashCard */
-         if(requestedId.equals(99L)){
-             return ResponseEntity.ok(new CashCard(99L, 123.45));
-         }else return ResponseEntity.notFound().build();
+       Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+
+        if(cashCardOptional.isPresent()){
+            return ResponseEntity.ok(cashCardOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+       /*
+       * OR with functional style expression
+       *
+       * return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+       * */
+
     }
 }
