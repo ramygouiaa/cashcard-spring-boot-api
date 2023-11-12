0d57f23d31d213ea318f0ac52e1edde5c66ea8d6
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-11-12 14:22:58 +0100

    adding update + test

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -71,6 +72,14 @@ public class CashCardController {
                         pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
+    }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<HttpStatus> updateCashCard(
+            @PathVariable Long requestedId,
+            @RequestBody CashCard cashCardUpdate) {
+
+        return ResponseEntity.noContent().build();
     }
 
 
