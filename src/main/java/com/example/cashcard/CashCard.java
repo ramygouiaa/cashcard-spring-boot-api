07ca8bdfa1a294c9d0d7852a685abfef750441b8
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2024-09-08 11:35:20 +0100

    bug fix

--- src/main/java/com/example/cashcard/CashCard.java
+++ src/main/java/com/example/cashcard/CashCard.java
@@ -3,6 +3,8 @@ package com.example.cashcard;
 import org.springframework.data.annotation.Id;
 import org.springframework.hateoas.RepresentationModel;
 
+import java.util.Objects;
+
 public class CashCard {
 
     @Id Long id;
@@ -25,5 +27,18 @@ public class CashCard {
 
     public String getOwner() {
         return owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CashCard cashCard = (CashCard) o;
+        return Objects.equals(id, cashCard.id) && Objects.equals(amount, cashCard.amount) && Objects.equals(owner, cashCard.owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount, owner);
     }
 }
